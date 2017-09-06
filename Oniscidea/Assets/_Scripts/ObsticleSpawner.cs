90b6140b991caaf0d51c2430cfb3db7a517f1141
Edward Healy <s00147364@mail.itsligo.ie>
2017-09-06 11:02:27 +0100

    Seventh_Commit Blob asset added

--- Oniscidea/Assets/_Scripts/ObsticleSpawner.cs
+++ Oniscidea/Assets/_Scripts/ObsticleSpawner.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class ObsticleSpawner : MonoBehaviour {
 
+    [SerializeField]
+    public GameObject TrunkObj;
+    [SerializeField]
+    public GameObject SapBlobObj;
 	// Use this for initialization
 	void Start ()
     {
-		
+        CreateTrunk();
+        CreateTrunk();
+        CreateTrunk();
+        CreateSapBlob();
+        CreateSapBlob();
+        CreateSapBlob();
 	}
 
     //private void OnBeforeTransformParentChanged()
-    void CreateTree()
+    void CreateTrunk()
     {
-        //var tree = Instantiate(TreeTrunk);
-        //tree.transform.parent = transform;
-        //tree.transform.Translate(Random.Range(-10,))
+        var tree = Instantiate(TrunkObj);
+        tree.transform.parent = transform;
+        tree.transform.Translate(Random.Range(-30,30),0,Random.Range(-5,5));
+    }
+    void CreateSapBlob()
+    {
+        var blob = Instantiate(SapBlobObj);
+        blob.transform.parent = transform;
+        blob.transform.Translate(Random.Range(-20, 20), 0, Random.Range(-5, 5));
     }
 
     // Update is called once per frame
